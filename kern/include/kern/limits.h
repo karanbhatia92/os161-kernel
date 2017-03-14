2cd266ffecf59e4611faa16fb3cd2f8def1590dd
karan.bhatia <karangop@buffalo.edu>
2017-03-13 20:04:17 -0400

    Stupid commit Exec implementation

--- kern/include/kern/limits.h
+++ kern/include/kern/limits.h
@@ -80,7 +80,7 @@
 #define __PID_MAX       32767
 
 /* Max open files per process */
-#define __OPEN_MAX      50
+#define __OPEN_MAX      128
 
 /* Max bytes for atomic pipe I/O -- see description in the pipe() man page */
 #define __PIPE_BUF      512
